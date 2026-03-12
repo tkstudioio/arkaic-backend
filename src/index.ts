aa97869795672d85424e7cd34def5006bca2cfe5
Michele Guidetti <contact@tkstudio.io>
2026-03-12 21:08:35 +0100

    moved old code to v1

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,39 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { products } from "./routes/products/index.js";
-import { chats } from "./routes/chats/index.js";
-import { escrows } from "./routes/escrows/index.js";
-import { config } from "./routes/config.js";
-import { account } from "./routes/account/index.js";
-import { auth } from "./routes/auth/index.js";
+import { products } from "./routes/v1/products/index.js";
+import { chats } from "./routes/v1/chats/index.js";
+import { escrows } from "./routes/v1/escrows/index.js";
+import { config } from "./routes/v1/config.js";
+import { account } from "./routes/v1/account/index.js";
+import { auth } from "./routes/v1/auth/index.js";
 
-const app = new Hono();
+const v1 = new Hono();
 
-app.use("*", cors());
+v1.use("*", cors());
 
-app.route("/products", products);
-app.route("/chats", chats);
-app.route("/escrows", escrows);
-app.route("/account", account);
-app.route("/config", config);
-app.route("/auth", auth);
+v1.route("/products", products);
+v1.route("/chats", chats);
+v1.route("/escrows", escrows);
+v1.route("/account", account);
+v1.route("/config", config);
+v1.route("/auth", auth);
+
+const v2 = new Hono();
+
+v2.use("*", cors());
+
+v2.route("/products", products);
+v2.route("/chats", chats);
+v2.route("/escrows", escrows);
+v2.route("/account", account);
+v2.route("/config", config);
+v2.route("/auth", auth);
 
 const port = Number(process.env.PORT) || 3000;
 
-serve({ fetch: app.fetch, port }, async () => {
+serve({ fetch: v2.fetch, port }, async () => {
   console.log(`Server running on port ${port}`);
 });
 
-export default app;
+export default v2;
