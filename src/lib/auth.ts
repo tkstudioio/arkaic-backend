eec0c5cd9bc18c9ffbb00c42af2e4e3c7f8ab00e
Michele Guidetti <contact@tkstudio.io>
2026-03-13 07:52:27 +0100

    feat(auth): add bearer auth, JWT validation, and schnorr signature verification middleware

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,43 @@
+import { toXOnly } from "./escrow";
+import { schnorr } from "./schnorr";
+import { hex } from "@scure/base";
 import { createMiddleware } from "hono/factory";
+import { decode } from "hono/jwt";
+import { prisma } from "./prisma";
 
-export type AuthEnv = { Variables: { pubkey: string } };
+export type AuthEnv = { Variables: { pubkey: string; signature: string } };
 
 export const bearerAuth = createMiddleware<AuthEnv>(async (c, next) => {
   const jwt = c.req.header("Authorization")?.replace("Bearer ", "");
 
-  console.log(jwt);
+  if (!jwt) return c.text("Missing Bearer token in request", 401);
+
+  const { payload } = decode(jwt);
+
+  const account = await prisma.account.findUnique({
+    where: { pubkey: payload.sub as string },
+  });
+  if (!account) return c.text("Account not found", 401);
+
+  c.set("pubkey", account.pubkey);
+  await next();
+});
+
+export const verifySignature = createMiddleware<AuthEnv>(async (c, next) => {
+  const pubkey = c.get("pubkey");
+  const body = await c.req.json();
+  const { signature, ...values } = body;
+
+  if (!signature) return c.text("Missing signature", 400);
+
+  const isValid = schnorr.verify(
+    hex.decode(signature),
+    new TextEncoder().encode(JSON.stringify(values)),
+    toXOnly(hex.decode(pubkey)),
+  );
+
+  if (!isValid) return c.text("Invalid signature", 401);
+
+  c.set("signature", signature);
   await next();
 });
---
+++ src/lib/schnorr.ts
@@ -0,0 +1,3 @@
+import { schnorr as schnorrUtil } from "@noble/curves/secp256k1";
+
+export const schnorr = schnorrUtil;
